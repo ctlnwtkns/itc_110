bedbc27d25cc3d3185bd1697f68ad300629faa2e
ctlnwtkns <ctlnwtkns@gmail.com>
2016-01-13 20:40:10 -0800

    add while and elif example files

---
+++ abs_ex/vampire2.py
@@ -0,0 +1,10 @@
+name = 'not Alice'
+age = 1000
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif int(age) > 100:
+    print('You are not Alice, grannie.')
+elif int(age) > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
