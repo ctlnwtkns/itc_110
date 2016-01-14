68703e9cd8f34c5b5ca24938345d874f55617317
ctlnwtkns <ctlnwtkns@gmail.com>
2016-01-13 20:59:39 -0800

    modify elif examples

--- abs_ex/vampire.py
+++ abs_ex/vampire.py
@@ -1,10 +1,12 @@
-name = 'not Alice'
-age = 1000
+print('What is your name?')
+name = input()
+print('How old are you?')
+age = input()
 if name == 'Alice':
     print('Hi, Alice.')
-elif age < 12:
+elif int(age) < 13:
     print('You are not Alice, kiddo.')
-elif age < 2000:
+elif int(age) > 2000:
     print('Unlike you, Alice is not an undead, immortal vampire.')
-elif age > 100:
+elif int(age) > 100:
     print('You are not Alice, grannie.')
