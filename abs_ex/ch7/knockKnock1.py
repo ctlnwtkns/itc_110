6c53f6d3c7886065eece481c1dfe233748255d55
ctlnwtkns <ctlnwtkns@gmail.com>
2016-02-24 19:01:54 -0800

    abs_hw ch7, regex knockKnock joke

--- abs_ex/ch7/knockKnock1.py
+++ abs_ex/ch7/knockKnock1.py
@@ -1,20 +1,32 @@
 import re #import the regex module
 
 def joke():
-    print('knock knock')
-    response = re.compile(input()) #create a regex object with the re.compile() function. Remember to use a raw string
-    print(response)
-    mo = response.search('Who\'s there?') #pass the desired string into the regex object's search() method. This returns a Match object
-    print('match object = ' + str(mo)) # call the Match object's group() method to return a string of the actual matched text
-    
-    '''if mo:
-        print('To.')
-        response2 = re.compile(input())
-        if response2 == response.search('To who?'):
-            print('To WHOM.')
-        else:
-            print('Try again.')
-            joke()'''
-
+	print 'knock knock'
+	response = raw_input()
+	'''answerRegex = re.compile(r'[W|w]ho[s|(\')?](\w)? [T|t]he(i)?r(e)?(\?)*')'''
+	answerRegex = re.compile(r'who[s|(\')?](\w)? the(i)?r(e)?(\?)*', re.I)
+	mo = answerRegex.search(response)
+	if mo != None:
+		if response in mo.group():
+			print('To.')
+			response2 = raw_input()
+			answerRegex2 = re.compile(r'to who(\?)*', re.I)
+			mo2 = answerRegex2.search(response2)
+			if response2 in mo2.group():
+				print('To WHOM.')
+			
+		
 joke()
         
+
+
+'''
+else:
+			print('Ugh! Seriously? Try again.')
+			joke()
+	else:else: 
+			print('Don\'t you know how this works? Try again.')
+    		joke()
+
+		
+    	'''
