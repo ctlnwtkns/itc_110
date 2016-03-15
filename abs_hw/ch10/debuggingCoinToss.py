b5e7316df8e199a1a46482ec05bd67cd93211435
Caitlin <ctlnwtkns@gmail.com>
2016-03-15 10:41:48 -0700

    finish hw

--- abs_hw/ch10/debuggingCoinToss.py
+++ abs_hw/ch10/debuggingCoinToss.py
@@ -4,19 +4,19 @@ guess = ''
 while guess not in ('heads', 'tails'):
 	print('Guess the coin toss! Enter heads or tails:')
 	guess = input()
-toss = random.randint(0,1) # 0 is tails, 1 is heads
+toss = random.choice(['heads','tails']) # 0 is tails, 1 is heads
 if toss == guess:
 	print('You got it!')
 else:
 	print('Nope! Guess again.')
-	guesss = input()
+	guess = input()
 	if toss == guess:
 		print('You got it!')
 	else:
 		print('Nope. You are really bad at this game.')
 	
 '''
-line 6 --> line 3 --> NameError --> must enter "'heads'" or "'tails'" 
+line 6 --> line 3 --> NameError --> must enter "'heads'" or "'tails'" (this is a python 2.7 issue...input() needs to be changed to raw_input())
 line 8 --> "toss" (which will only ever be 0 or 1) can never equal "guess" (which should be "heads" or "tails"); the comment needs to be translated into code
 line 12 --> misspelled guess(s)
 '''
